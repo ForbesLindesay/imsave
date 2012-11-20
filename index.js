7aa12a5804dd49c0d07ec693312a6bc17fe9309e
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-11-20 20:18:32 +0000

    Make all errors live in the promise

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 var promise = require('promises-a');
 var emitter = require('emitter');
 
+// Get your own key: http://api.imgur.com/
 module.exports = imgur;
 function imgur(apiKey) {
   function upload(file) {
     var def = promise();
     emitter(def.promise);
-
-    if (!file || !file.type.match(/image.*/))
-      throw new Error('Invalid file type, imgur only accepts images.');
-
-    var fd = new FormData();
-    fd.append("image", file); // Append the file
-    fd.append("key", apiKey);
-    // Get your own key: http://api.imgur.com/
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://api.imgur.com/2/upload.json"); // Boooom!
-    xhr.onload = function () {
-      try {
-        def.fulfill(JSON.parse(xhr.responseText));
-      } catch (ex) {
-        def.reject(ex);
+    try {
+      if (!file || !file.type.match(/image.*/)) {
+        var err = new Error('Invalid file type, imgur only accepts images.');
+        err.code = 'InvalidFileType';
+        throw err;
       }
-    }
-    xhr.onerror = def.reject;
-    xhr.upload.onprogress = function (e) {
-      e.perecent = e.loaded / e.total * 100;
-      def.promise.emit('progress', e);
-    }
-    def.promise.abort = function () {
-      xhr.abort();
 
-    };
+      var fd = new FormData();
+      fd.append("image", file); // Append the file
+      fd.append("key", apiKey);
+      
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://api.imgur.com/2/upload.json"); // Boooom!
+      xhr.onload = function () {
+        try {
+          def.fulfill(JSON.parse(xhr.responseText).upload);
+        } catch (ex) {
+          def.reject(ex);
+        }
+      }
+      xhr.onerror = def.reject;
+      xhr.upload.onprogress = function (e) {
+        e.perecent = e.loaded / e.total * 100;
+        def.promise.emit('progress', e);
+      }
+      def.promise.abort = function () {
+        xhr.abort();
+      };
 
-    xhr.send(fd);
+      xhr.send(fd);
+    } catch (ex) {
+      def.reject(ex);
+    }
     return def.promise;
   }
   return {upload: upload};
